253657ef5c35615dbfa43e32eb6dcd8568dc20a4
Julian Slatlem <julianahslatlem@gmail.com>
2023-10-30 14:02:39 +0100

    Added individual logs

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ function openHome() {
     document.getElementById("page").src = "home.html";
 }
 
-function openLog() {
-    document.getElementById("page").src = "log.html";
-}
-
 function openGit() {
     document.getElementById("page").src = "git.html";
 }
 
 function openGitHub() {
     document.getElementById("page").src = "github.html";
+}
+
+function openLog() {
+    document.getElementById("page").src = "log.html";
+}
+
+function openJulianLog() {
+    document.getElementById("page").src = "julianlog.html";
+}
+
+function openBenjaminLog() {
+    document.getElementById("page").src = "benjaminlog.html";
+}
+
+function openMortenLog() {
+    document.getElementById("page").src = "mortenlog.html";
+}
+
+function openDanielLog() {
+    document.getElementById("page").src = "daniellog.html";
 }
