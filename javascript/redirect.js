a3332fea947aa8f40d05f40f0a62fafd5754f08c
Julian Slatlem <julianahslatlem@gmail.com>
2023-11-04 22:13:20 +0100

    Removed iframes

--- javascript/redirect.js
+++ javascript/redirect.js
@@ -1,11 +1,11 @@
-setTimeout(function() {
+function openPage(path) {
     var currentURL = window.location.href;
 
     var urlParts = currentURL.split('/');
 
-    urlParts[urlParts.length - 1] = 'pages/home.html';
+    urlParts[urlParts.length - 1] = path;
 
     var newURL = urlParts.join('/');
 
     window.location.href = newURL;
-});
+}
